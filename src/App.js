241634838927dfaca0d966cfb9c8b5676d318524
Roheem Olayemi <abdurroheem20@gmail.com>
2020-09-27 10:21:55 +0100

    made some adjustments

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ const App = () => {
 
   const APP_ID = "37e53402"
 
-  const APP_KEY = "6308be23a473f83d604ee2fdf904734e"
-  const API_URL = `https://api.edamam.com/search?q=banana&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const API_KEY = "6308be23a473f83d604ee2fdf904734e"
 
-  const [recipe, setRecipes] = useState([])
+  const API_URL = `https://api.edamam.com/search?q=banana&app_id=${APP_ID}&app_key=${API_KEY}`
+
+  // console.log(process.env.REACT_APP_RECIPE_API_KEY, process.env.REACT_APP_RECIPE_APP_ID)
+  const [recipes, setRecipes] = useState([])
 
   useEffect(() => {
     loadData();
@@ -18,17 +20,17 @@ const App = () => {
 
   const loadData = async () => {
     const response = await fetch(API_URL)
-
+    // console.log(API_URL);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    console.log(data.hits); 
   }
 
   return (
     <div className="App">
     {
-       recipe.map((recipes,id) => (
-         <Recipe key={id} title={recipes.recipe.label} image={recipes.recipe.image} calories={recipes.recipe.calories} />
+       recipes.map((recipe,id) => (
+         <Recipe key={id} title={recipe.recipe} image={recipe.recipe.image} calories={recipe.recipe.calories} />
        ))
      }
    </div>
